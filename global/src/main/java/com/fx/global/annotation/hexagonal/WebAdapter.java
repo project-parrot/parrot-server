473950613dd79b13ea624e2a32ba071ce1fabfa3
SEOB <svj7531@naver.com>
2025-07-31 17:58:59 +0900

    feat: 공통 Annotation 개발
    feat: 공통 Annotation 개발

---
+++ global/src/main/java/com/fx/global/annotation/hexagonal/WebAdapter.java
@@ -0,0 +1,14 @@
+package com.fx.global.annotation.hexagonal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.web.bind.annotation.RestController;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@RestController
+public @interface WebAdapter {
+
+}
