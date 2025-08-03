c99fbf492877f88d3603b9f8723ca6ab0abc438b
SEOB <svj7531@naver.com>
2025-08-03 15:13:04 +0900

    kotlin: global 모듈 코틀린으로 변경

---
+++ global/src/main/kotlin/com/fx/global/annotation/hexagonal/CacheAdapter.kt
@@ -0,0 +1,9 @@
+package com.fx.global.annotation.hexagonal
+
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Component
+annotation class CacheAdapter {
+}
---
+++ global/src/main/kotlin/com/fx/global/api/Api.kt
@@ -0,0 +1,81 @@
+package com.fx.global.api
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+class Api<T>(
+    val success: Boolean,
+    val code: Int,
+    val message: String?,
+    val data: T?
+) {
+
+    companion object {
+        @JvmStatic
+        fun <T> OK(data: T, message: String): ResponseEntity<Api<T>> =
+            ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                    Api(
+                        success = true,
+                        code = HttpStatus.OK.value(),
+                        message = message,
+                        data = data
+                    )
+                )
+
+        @JvmStatic
+        fun <T> OK(data: T): ResponseEntity<Api<T>> =
+            ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                    Api(
+                        success = true,
+                        code = HttpStatus.OK.value(),
+                        message = null,
+                        data = data
+                    )
+                )
+
+        @JvmStatic
+        fun <T> OK(data: T, httpStatus: HttpStatus): ResponseEntity<Api<T>> =
+            ResponseEntity
+                .status(httpStatus)
+                .body(
+                    Api(
+                        success = true,
+                        code = httpStatus.value(),
+                        message = null,
+                        data = data
+                    )
+                )
+
+        @JvmStatic
+        fun <T> OK(data: T, message: String?, httpStatus: HttpStatus): ResponseEntity<Api<T>> =
+            ResponseEntity
+                .status(httpStatus)
+                .body(
+                    Api(
+                        success = true,
+                        code = httpStatus.value(),
+                        message = message,
+                        data = data
+                    )
+                )
+
+        @JvmStatic
+        fun ERROR(errorCodeIfs: ErrorCodeIfs): ResponseEntity<Api<ErrorCodeIfs>> =
+            ResponseEntity
+                .status(errorCodeIfs.httpStatus)
+                .body(
+                    Api(
+                        success = false,
+                        code = errorCodeIfs.httpStatus.value(),
+                        message = errorCodeIfs.message,
+                        data = null
+                    )
+                )
+    }
+
+
+}
