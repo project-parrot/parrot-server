cce1e5d584802618369800c0ded6ebc42821d36f
Baesiru <pingol3397@gmail.com>
2025-08-10 01:32:14 +0900

    feat: 게시글 작성에 인가 객체 추가
    feat: 게시글 작성에 인가 객체 추가
    
    - PostApiAdapter에 인수로 AuthUser 추가
    - PostCreateRequest에 userId 필드 삭제
    - PostCreateRequest의 toCommand의 인수로 AuthUser를 받고 userId값으로 커맨드 객체 생성

--- post/src/main/kotlin/com/fx/post/adapter/in/web/dto/PostCreateRequest.kt
+++ post/src/main/kotlin/com/fx/post/adapter/in/web/dto/PostCreateRequest.kt
@@ -1,18 +1,17 @@
 package com.fx.post.adapter.`in`.web.dto
 
+import com.fx.global.resolver.AuthUser
 import com.fx.post.application.`in`.dto.PostCreateCommand
 import jakarta.validation.constraints.Size
 
 data class PostCreateRequest(
-    // 임시로 userId를 직접 받음
-    val userId: Long,
     @field:Size(max = 2000, message = "게시글은 2000자 이내여야 합니다.")
     val content: String,
     //val mediaIds: List<Long>
 ) {
-    fun toCommand(): PostCreateCommand {
+    fun toCommand(authUser: AuthUser): PostCreateCommand {
         return PostCreateCommand(
-            userId = this.userId,
+            userId = authUser.userId,
             content = this.content,
             //mediaIds = this.mediaIds
         )
--- post/src/main/kotlin/com/fx/post/application/in/dto/PostCreateCommand.kt
+++ post/src/main/kotlin/com/fx/post/application/in/dto/PostCreateCommand.kt
@@ -1,7 +1,7 @@
 package com.fx.post.application.`in`.dto
 
+
 data class PostCreateCommand(
-    //임시로 유저아이디를 받음
     val userId: Long,
     val content: String? = null,
     //val mediaIds: List<Long>
