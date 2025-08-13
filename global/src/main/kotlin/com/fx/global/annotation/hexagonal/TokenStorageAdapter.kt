3110557004d7a50c6a759f1b17020c060381ec20
Baesiru <pingol3397@gmail.com>
2025-08-13 14:22:20 +0900

    refactor: TokenStoarageAdapter 수정
    refactor: TokenStoarageAdapter 수정

--- global/src/main/kotlin/com/fx/global/annotation/hexagonal/TokenStorageAdapter.kt
+++ global/src/main/kotlin/com/fx/global/annotation/hexagonal/TokenStorageAdapter.kt
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @Component
-annotation class TokenStorageDXcAdapter {
+annotation class TokenStorageAdapter {
 }
