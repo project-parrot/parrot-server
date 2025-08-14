e47d7880de41d0463dd5779c560eae30aa6a9306
SEOB <svj7531@naver.com>
2025-08-14 13:14:59 +0900

    feat: 팔로우 승인 API 개발

--- user/src/main/kotlin/com/fx/user/application/service/FollowCommandService.kt
+++ user/src/main/kotlin/com/fx/user/application/service/FollowCommandService.kt
@@ -60,6 +60,14 @@ class FollowCommandService(
         return true
     }
 
+    @Transactional
+    override fun approveFollowUser(userId: Long, followId: Long): Follow {
+        val follow = followPersistencePort.getFollow(followId)?: throw FollowException(FollowErrorCode.FOLLOW_NOT_FOUND)
+
+        // 이미 Approved 상태인 경우 예외
+        if (follow.status == FollowStatus.APPROVED) {
+            throw FollowException(FollowErrorCode.ALREADY_FOLLOWING)
+        }
 
         // 자신에게 온 following 인지 확인
         if (follow.followingId != userId) {
