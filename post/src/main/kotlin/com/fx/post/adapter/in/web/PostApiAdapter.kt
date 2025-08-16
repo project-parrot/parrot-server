f969e9b39e66dfe32aa9bebe4277e0f9e0772b4d
Baesiru <pingol3397@gmail.com>
2025-08-16 11:16:40 +0900

    refactor: Post모듈의 PostApiAdapter 리팩토링
    refactor: Post모듈의 PostApiAdapter 리팩토링

--- post/src/main/kotlin/com/fx/post/adapter/in/web/PostApiAdapter.kt
+++ post/src/main/kotlin/com/fx/post/adapter/in/web/PostApiAdapter.kt
@@ -7,7 +7,6 @@ import com.fx.global.resolver.AuthUser
 import com.fx.post.adapter.`in`.web.dto.post.*
 import com.fx.post.application.`in`.PostCommandUseCase
 import com.fx.post.application.`in`.PostQueryUseCase
-import com.fx.post.application.out.web.MediaWebPort
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.*
 class PostApiAdapter(
     private val postCommandUseCase: PostCommandUseCase,
     private val postQueryUseCase: PostQueryUseCase,
-    private val mediaWebPort: MediaWebPort
 ) {
     @PostMapping
     fun createPost(
@@ -31,36 +29,38 @@ class PostApiAdapter(
     @GetMapping
     fun getFollowersPosts(@RequestParam postId: Long = Long.MAX_VALUE,
                           @AuthenticatedUser authUser: AuthUser
-    ): ResponseEntity<Api<List<PostResponse>>> {
-        val posts = postQueryUseCase.getFollowersPosts(authUser.userId, postId)
-        return Api.OK(posts.map { PostResponse.from(it) })
-    }
+    ): ResponseEntity<Api<List<PostResponse>>> =
+        Api.OK(
+            postQueryUseCase.getFollowersPosts(authUser.userId, postId)
+                .map { PostResponse.from(it) }
+        )
 
     @GetMapping("/me")
     fun getMyPosts(@RequestParam postId: Long = Long.MAX_VALUE,
                  @AuthenticatedUser authUser: AuthUser
-    ): ResponseEntity<Api<List<PostResponse>>> {
-        val posts = postQueryUseCase.getMyPosts(authUser.userId, postId)
-        return Api.OK(posts.map { PostResponse.from(it) })
-    }
+    ): ResponseEntity<Api<List<PostResponse>>> =
+        Api.OK(
+            postQueryUseCase.getMyPosts(authUser.userId, postId)
+                .map { PostResponse.from(it) }
+        )
 
     @GetMapping("/{userId}")
     fun getUserPosts(@PathVariable userId: Long,
                      @RequestParam postId: Long = Long.MAX_VALUE
-    ): ResponseEntity<Api<List<PostResponse>>> {
-        val posts = postQueryUseCase.getUserPosts(userId, postId)
-        return Api.OK(posts.map { PostResponse.from(it) })
-    }
-
+    ): ResponseEntity<Api<List<PostResponse>>> =
+        Api.OK(
+            postQueryUseCase.getUserPosts(userId, postId)
+                .map { PostResponse.from(it) }
+        )
 
     @PutMapping("/{postId}")
     fun updatePost(
         @PathVariable postId: Long,
         @RequestBody @Valid postUpdateRequest: PostUpdateRequest,
         @AuthenticatedUser authUser: AuthUser
-    ): ResponseEntity<Api<PostUpdateResponse>> {
-        val post = postCommandUseCase.updatePost(postId, postUpdateRequest.toCommand(authUser))
-        return Api.OK(PostUpdateResponse(post.id))
-    }
+    ): ResponseEntity<Api<PostUpdateResponse>> =
+        Api.OK(PostUpdateResponse(
+            postCommandUseCase.updatePost(postId, postUpdateRequest.toCommand(authUser)).id)
+        )
 
 }
