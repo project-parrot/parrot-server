8be09917defc0ae949d821a6967e51a933cfcec7
Baesiru <pingol3397@gmail.com>
2025-08-19 08:47:33 +0900

    feat: PostApiAdapter Swagger operation 설정
    feat: PostApiAdapter Swagger operation 설정

--- post/src/main/kotlin/com/fx/post/adapter/in/web/PostApiAdapter.kt
+++ post/src/main/kotlin/com/fx/post/adapter/in/web/PostApiAdapter.kt
@@ -7,6 +7,7 @@ import com.fx.global.resolver.AuthUser
 import com.fx.post.adapter.`in`.web.dto.post.*
 import com.fx.post.application.`in`.PostCommandUseCase
 import com.fx.post.application.`in`.PostQueryUseCase
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -20,6 +21,8 @@ class PostApiAdapter(
     private val postCommandUseCase: PostCommandUseCase,
     private val postQueryUseCase: PostQueryUseCase,
 ) {
+
+    @Operation(summary = "게시글 작성")
     @PostMapping
     fun createPost(
         @RequestBody @Valid postCreateRequest: PostCreateRequest,
@@ -33,6 +36,10 @@ class PostApiAdapter(
             )
         )
 
+    @Operation(summary = "내가 팔로워한 사용자 게시글 조회",
+        description = "default : [sort=id,DESC], [size:20] <br>" +
+        " 요청 예시 : /api/v1/posts?sort=id,DESC&size=20"
+    )
     @GetMapping()
     fun getFollowersPosts(
         @AuthenticatedUser authUser: AuthUser,
@@ -47,7 +54,10 @@ class PostApiAdapter(
             )
         )
 
-
+    @Operation(summary = "내 게시글 조회",
+        description = "default : [sort=id,DESC], [size:20] <br>" +
+                " 요청 예시 : /api/v1/posts/me?sort=id,DESC&size=20"
+    )
     @GetMapping("/me")
     fun getMyPosts(
         @AuthenticatedUser authUser: AuthUser,
@@ -62,6 +72,10 @@ class PostApiAdapter(
             )
         )
 
+    @Operation(summary = "특정 사용자 게시글 조회",
+        description = "default : [sort=id,DESC], [size:20] <br>" +
+                " 요청 예시 : /api/v1/posts/{userId}?sort=id,DESC&size=20"
+    )
     @GetMapping("/{userId}")
     fun getUserPosts(
         @AuthenticatedUser authUser: AuthUser,
@@ -77,6 +91,10 @@ class PostApiAdapter(
             )
         )
 
+    @Operation(summary = "게시글 수정",
+        description = "Body의 mediaIds에 따라 해당 게시글의 PostMedia가 수정되므로 주의 <br>" +
+            "기존 mediaIds=[1, 2]가 일 때 body의 mediaIds=[1, 3]로 보내면 2는 지워지고 3은 추가됨"
+    )
     @PutMapping("/{postId}")
     fun updatePost(
         @PathVariable postId: Long,
