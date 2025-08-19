1cd395f4e8bdeb3649036b3ff8b974021452d6cc
Baesiru <pingol3397@gmail.com>
2025-08-19 09:55:37 +0900

    feat: 게시글 댓글 조회 필드 추가 및 Swagger Operation 추가
    feat: 게시글 댓글 조회 필드 추가 및 Swagger Operation 추가

--- post/src/main/kotlin/com/fx/post/adapter/in/web/dto/comment/CommentResponse.kt
+++ post/src/main/kotlin/com/fx/post/adapter/in/web/dto/comment/CommentResponse.kt
@@ -1,30 +1,33 @@
 package com.fx.post.adapter.`in`.web.dto.comment
 
 import com.fx.post.domain.Comment
+import com.fx.post.domain.CommentInfo
 import java.time.LocalDateTime
 
 data class CommentResponse(
     val commentId: Long?= null,
     val userId: Long,
-    //val nickname: String,
-    //val profileImageUrl: String?= null,
+    val nickname: String?= null,
+    val profileImageUrl: String?= null,
     val content: String,
     val createdAt: LocalDateTime?= null,
     val parentId: Long?= null
 ) {
     companion object {
-        fun from(comment: Comment): CommentResponse {
+        fun from(commentInfo: CommentInfo): CommentResponse {
             return CommentResponse(
-                commentId = comment.id,
-                userId = comment.userId,
-                content = comment.content,
-                createdAt = comment.createdAt,
-                parentId = comment.parentId
+                commentId = commentInfo.id,
+                userId = commentInfo.userId,
+                nickname = commentInfo.nickname,
+                profileImageUrl = commentInfo.profileImageUrl,
+                content = commentInfo.content,
+                createdAt = commentInfo.createdAt,
+                parentId = commentInfo.parentId
             )
         }
 
-        fun from(commentList: List<Comment>): List<CommentResponse> {
-            return commentList.map { from(it) }
+        fun from(commentInfoList: List<CommentInfo>): List<CommentResponse> {
+            return commentInfoList.map { from(it) }
         }
     }
 }
