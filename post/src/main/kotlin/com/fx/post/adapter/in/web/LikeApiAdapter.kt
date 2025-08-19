70c4220970838b8a76587528118140f33a79bd79
Baesiru <pingol3397@gmail.com>
2025-08-19 09:26:53 +0900

    feat: LikeApiAdapter Swagger operation 설정
    feat: LikeApiAdapter Swagger operation 설정

--- post/src/main/kotlin/com/fx/post/adapter/in/web/LikeApiAdapter.kt
+++ post/src/main/kotlin/com/fx/post/adapter/in/web/LikeApiAdapter.kt
@@ -10,6 +10,7 @@ import com.fx.post.adapter.`in`.web.dto.post.PostResponse
 import com.fx.post.application.`in`.LikeCommandUseCase
 import com.fx.post.application.`in`.LikeQueryUseCase
 import com.fx.post.application.`in`.dto.LikeQueryCommand
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
@@ -23,6 +24,7 @@ class LikeApiAdapter(
     private val likeQueryUseCase: LikeQueryUseCase
 ) {
 
+    @Operation(summary = "좋아요 추가")
     @PostMapping("/{postId}/likes")
     fun addLike(
         @PathVariable postId: Long,
@@ -32,6 +34,7 @@ class LikeApiAdapter(
             likeCommandUseCase.addLike(postId, authUser.userId)
         )
 
+    @Operation(summary = "좋아요 취소")
     @DeleteMapping("/{postId}/likes")
     fun cancelLike(
         @PathVariable postId: Long,
@@ -41,6 +44,7 @@ class LikeApiAdapter(
             likeCommandUseCase.cancelLike(postId, authUser.userId)
         )
 
+    @Operation(summary = "게시글을 좋아요 한 사용자 목록 조회")
     @GetMapping("/{postId}/likes")
     fun getLikeUsers(
         @PathVariable postId: Long
@@ -49,9 +53,12 @@ class LikeApiAdapter(
             LikeUsersResponse.from(likeQueryUseCase.getLikeUsers(postId))
         )
 
-
+    @Operation(summary = "내가 좋아요한 게시글 목록 조회",
+        description = "default : [sort=id,DESC], [size:20] <br>" +
+                " 요청 예시 : /api/v1/posts/me/likes?sort=id,DESC&size=20"
+    )
     @GetMapping("/me/likes")
-    fun getMyLikedPostsDsl(
+    fun getMyLikedPosts(
         @AuthenticatedUser authUser: AuthUser,
         @ModelAttribute likeSearchParam: LikeSearchParam,
         @PageableDefault(sort = ["id"], direction = Sort.Direction.DESC, size = 20) pageable: Pageable
