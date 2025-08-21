76446433a25805695c1ae2b2ef7ed3c7267ecd4d
SEOB <svj7531@naver.com>
2025-08-22 01:12:58 +0900

    refactor: createdAt -> id 기반으로 페이징 조회
    refactor: createdAt -> id 기반으로 페이징 조회

--- user/src/main/kotlin/com/fx/user/adapter/in/web/dto/follow/FollowSearchParam.kt
+++ user/src/main/kotlin/com/fx/user/adapter/in/web/dto/follow/FollowSearchParam.kt
@@ -2,11 +2,9 @@ package com.fx.user.adapter.`in`.web.dto.follow
 
 import com.fx.user.application.`in`.dto.FollowQueryCommand
 import org.springframework.data.domain.Pageable
-import java.time.LocalDateTime
 
 data class FollowSearchParam(
     val followId: Long? = null,
-    val createdAt: LocalDateTime? = null,
     val nickname: String? = null
 ) {
 
@@ -15,7 +13,6 @@ data class FollowSearchParam(
             requestUserId = requestUserId,
             targetUserId = targetUserId,
             followId = this.followId,
-            createdAt = this.createdAt,
             nickname = this.nickname,
             pageable = pageable
         )
