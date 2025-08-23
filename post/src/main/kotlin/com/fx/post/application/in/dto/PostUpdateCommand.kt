65e3d1cbda3fec39155ff98296c9ca5191dda3e1
Baesiru <pingol3397@gmail.com>
2025-08-23 12:45:08 +0900

    refactor: Post 엔티티 관련 커맨드 유효성 검증 추가
    refactor: Post 엔티티 관련 커맨드 유효성 검증 추가
    
    - PostCreateCommand 유효성 검증 추가(userId 필드 NotNull)
    - PostUpdateCommand 유효성 검증 추가(userId, role 필드 NotNull)

--- post/src/main/kotlin/com/fx/post/application/in/dto/PostUpdateCommand.kt
+++ post/src/main/kotlin/com/fx/post/application/in/dto/PostUpdateCommand.kt
@@ -1,10 +1,15 @@
 package com.fx.post.application.`in`.dto
 
 import com.fx.global.dto.UserRole
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 
 class PostUpdateCommand(
+    @field:NotNull
     val userId: Long,
+    @field:NotNull
     val role: UserRole,
+    @field:Size(max = 2000, message = "게시글은 2000자 이내여야 합니다.")
     val content: String,
     val mediaIds: List<Long>?
 )
