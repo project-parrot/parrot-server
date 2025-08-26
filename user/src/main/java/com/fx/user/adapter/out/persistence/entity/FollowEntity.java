cab53dd7b1f6c7a9078864fcb61efe6c2d568cb0
SEOB <svj7531@naver.com>
2025-08-26 18:00:48 +0900

    feat: Follow 목록 조회 index 추가
    feat: Follow 목록 조회 index 추가

--- user/src/main/java/com/fx/user/adapter/out/persistence/entity/FollowEntity.java
+++ user/src/main/java/com/fx/user/adapter/out/persistence/entity/FollowEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -15,7 +16,12 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @Table(name = "follow",
     indexes = {
-        @Index(name = "idx_follower_following_status", columnList = "followerId, ,followingId status") // 특정 팔로우 존재 여부 확인 idx
+        @Index(name = "idx_follower_status_id", columnList = "followerId, status, id"),
+        @Index(name = "idx_following_status_id", columnList = "followingId, status, id"),
+        @Index(name = "idx_follower_following_status", columnList = "followerId, followingId, status") // 특정 팔로우 존재 여부 확인 idx
+    },
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_follower_following", columnNames = {"followerId", "followingId"})
     }
 )
 @SuperBuilder
